f203e430b8aee8bb7fa7c40c9423ee64fcffa501
StealtyPanda <42720778+StealthyPanda@users.noreply.github.com>
2021-07-10 18:43:14 +0400

    Updated Quaternions to now have multiplication

--- Quaternion.java
+++ Quaternion.java
@@ -29,10 +29,18 @@ public class Quaternion
 
 	}
 
+	public Quaternion(Vector3 imaginarypart)
+	{
+
+		this.w = 0;
+		this.imaginarypart = imaginarypart;
+
+	}
+
 	public Quaternion(double realpart, Vector3 imaginarypart)
 	{
 
-		this.w = w;
+		this.w = realpart;
 		this.imaginarypart = imaginarypart;
 
 	}
@@ -57,8 +65,22 @@ public class Quaternion
 	public static Quaternion multiply(Quaternion q1, Quaternion q2)
 	{
 
+		Quaternion pureconst = new Quaternion(q1.w * q2.w, 0, 0, 0);
+		Quaternion w1vect = new Quaternion(q2.imaginarypart.getMultiplied(q1.w));
+		Quaternion w2vect = new Quaternion(q1.imaginarypart.getMultiplied(q2.w));
+		Quaternion vectproduct = new Quaternion(Vector3.dotproduct(q1.imaginarypart, q2.imaginarypart) * -1, Vector3.crossproduct(q1.imaginarypart, q2.imaginarypart));
 		
+		Quaternion finalproduct = Quaternion.add( Quaternion.add( Quaternion.add(pureconst, w1vect), w2vect ), vectproduct );
+
+		return finalproduct;
 		
+	}
+
+	public String toString()
+	{
+		String retter = "";
+		retter += "(" + this.w + ", " + this.imaginarypart.x + ", " + this.imaginarypart.y + ", " + this.imaginarypart.z + ")";
+		return retter;
 	}
 
 }
