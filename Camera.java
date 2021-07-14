52cff80f16fd4d4c67d96044b1eed3ec5b8259f1
StealtyPanda <42720778+StealthyPanda@users.noreply.github.com>
2021-07-14 17:43:04 +0400

    Removed slight bugs in camera movement
    Camera movement is TRULY solid now.

--- Camera.java
+++ Camera.java
@@ -38,6 +38,15 @@ public class Camera
 		return plane;
 	}
 
+	public Camera() //testing purposes only
+	{
+		this.position = new Vector3(0, 0, 0);
+		this.orientation = new Quaternion(1, 0, 0, 0);
+		//this.targetworld = targetworld;
+		//this.rendergroup = this.targetworld.rendergroup;
+		sensor = generateSensor();
+	}
+
 	public Camera(World targetworld)
 	{
 		this.position = new Vector3(0, 0, 0);
@@ -87,6 +96,17 @@ public class Camera
 	{
 		this.orientation = Quaternion.multiply(this.orientation, Quaternion.getRotor(axis, angleinradians));
 		this.sensor.rotateAbout(this.position, axis, angleinradians);
+	}
+
+	public void printInfo()
+	{
+		System.out.println("Camera position: " + this.position.getClean().toString());
+		System.out.println("Camera orientation: " + this.orientation.getClean().toString());
+		System.out.println("Camera orientation vect: " + this.orientation.getVectorOrientation().getClean().toString());
+		System.out.println("Sensor position: " + this.sensor.position.getClean().toString());
+		System.out.println("Sensor orientation: " + this.sensor.normal.getClean().toString());
+		System.out.println("Sensor distance: " + Vector3.add(this.sensor.position, this.position.getMultiplied(-1)).getClean().toString());
+		System.out.println();
 	}
 
 
